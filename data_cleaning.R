2663e720de1d581ea773a326b0bb0221df2247b2
AleaSeifert <alea.seifert@gmail.com>
2024-04-24 09:58:28 -0700

    cleaned dataset

--- data_cleaning.R
+++ data_cleaning.R
@@ -7,3 +7,12 @@ animals <- read_csv(here("animal_rescues.csv"))
 # Converting data to excel
 library(writexl)
 writexl::write_xlsx(animals, "animals.xlsx")
+
+
+# Interested variables
+animals_new <- animals |> 
+  select(date_time_of_call, 
+         cal_year, 
+         animal_group_parent, 
+         special_service_type_category,
+         borough)
